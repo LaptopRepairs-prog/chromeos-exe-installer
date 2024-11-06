c51b33e6adf69c5840a74939b6b1d40868c52dd3
laptop repairs <139374267+LaptopRepairs-prog@users.noreply.github.com>
2024-11-06 21:27:28 +0000

    setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
+# update os
+sudo apt update -y
+
 # Install Wine
 sudo dpkg --add-architecture i386
 sudo wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
 sudo apt install --install-recommends winehq-stable
-
-# update os
-sudo apt update -y
 
 # Create.desktop file
 mkdir -p ~/.local/share/applications
