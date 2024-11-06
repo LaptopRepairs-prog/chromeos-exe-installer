cd36b8d241777a4722dad0ad33212a2ff1e26d69
Steve <139374267+robinokeeffe@users.noreply.github.com>
2024-11-06 20:35:49 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+# Install Wine
+sudo dpkg --add-architecture i386
+sudo wget -nc -O /usr/share/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key sudo apt update
+sudo apt install --install-recommends winehq-stable
+# Create.desktop file
+mkdir -p ~/.local/share/applications
